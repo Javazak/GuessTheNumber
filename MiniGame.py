86bfff650fb798030f20da1281f155835c407a1e
Javazak <panosyancode@gmail.com>
2020-02-18 00:17:54 +0400

    Here I'm added a count statement for counting the number of attempts

--- MiniGame.py
+++ MiniGame.py
@@ -1,5 +1,6 @@
 import random
 secretNumber = random.randint(0, 10)
+count = 0
 userInput = ""
 while userInput != secretNumber:
     userInput = int(input("Can you guess, what the number is it? "))
@@ -7,7 +8,9 @@ while userInput != secretNumber:
         print("Yeaaaaahhh, you find it!!!!!! ", "The secret number is ", secretNumber)
         break
     elif userInput > secretNumber:
+        count += 1
         print("Lower , lower )))")
     elif userInput < secretNumber:
+        count += 1
         print("Bigger , bigger ...")
 
